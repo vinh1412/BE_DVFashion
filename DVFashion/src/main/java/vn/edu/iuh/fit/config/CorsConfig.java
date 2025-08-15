4c8f3271a3d33dbbd05ce48f6b0d713b8cd4bc9d
Tran Hien Vinh <tranvinh141203@gmail.com>
2025-08-16 06:34:20 +0700

    [ADD] config cors

---
+++ DVFashion/src/main/java/vn/edu/iuh/fit/config/CorsConfig.java
@@ -0,0 +1,39 @@
+/*
+ * @ {#} CorsConfig.java   1.0     16/08/2025
+ *
+ * Copyright (c) 2025 IUH. All rights reserved.
+ */
+
+package vn.edu.iuh.fit.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
+
+/*
+ * @description: Configuration class for CORS settings
+ * @author: Tran Hien Vinh
+ * @date:   16/08/2025
+ * @version:    1.0
+ */
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.addAllowedOriginPattern("*");
+        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
