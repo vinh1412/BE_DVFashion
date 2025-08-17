d856374a5504a7b149f373a6dea461a72deff548
Duong-GB <duongnguyenqn1323@gmail.com>
2025-08-17 22:27:20 +0700

    [add] entities Address, Brand, Category

---
+++ DVFashion/src/main/java/vn/edu/iuh/fit/entities/Category.java
@@ -0,0 +1,45 @@
+/*
+ * @ {#} Category.java   1.0     8/17/2025
+ *
+ * Copyright (c) 2025 IUH. All rights reserved.
+ */
+
+package vn.edu.iuh.fit.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * @description: Entity class representing a category in the product catalog.
+ * @author: Nguyen Tan Thai Duong
+ * @date:   8/17/2025
+ * @version:    1.0
+ */
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private String image;
+
+    @Column(columnDefinition = "boolean default true")
+    private boolean active;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<Product> products = new ArrayList<>();
+}
+
