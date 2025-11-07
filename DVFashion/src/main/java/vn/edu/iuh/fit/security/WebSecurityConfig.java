aa6c8e92e32cfeb6a0517a2950624409a54e8a02
Nguyễn Tấn Thái Dương <100653357+DuongGB@users.noreply.github.com>
2025-11-07 10:00:22 +0700

    [fix] handle logic add to cart

--- DVFashion/src/main/java/vn/edu/iuh/fit/security/WebSecurityConfig.java
+++ DVFashion/src/main/java/vn/edu/iuh/fit/security/WebSecurityConfig.java
@@ -133,6 +133,7 @@ public class WebSecurityConfig {
                         .requestMatchers(HttpMethod.GET, path("/cart/**")).permitAll()
                         .requestMatchers(HttpMethod.GET, path("/reviews/product/**")).permitAll()
                         .requestMatchers(HttpMethod.GET, path("/addresses/provinces/**")).permitAll()
+                        .requestMatchers(HttpMethod.GET, path("/vouchers/customer")).permitAll()
                         .requestMatchers(HttpMethod.POST, path("/cart/**")).permitAll()
                         .requestMatchers( "/v3/api-docs/**",
                                 "/swagger-ui/**",
